27c7def80c9a3c5899d8dd8c3cf8a97949e052cf
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-02 02:11:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each do
 end
 
 def summon_captain_planet# code an argument here
