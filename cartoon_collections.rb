78b44e21215766bad87153fc0596a1e215689c3a
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-02 02:31:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
+  array.find do |cheese|
+      cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    end
 end
