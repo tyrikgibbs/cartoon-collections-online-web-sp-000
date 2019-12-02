2903e7eab908237aa418755f53812b572816a11b
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-02 02:23:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,11 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
+  planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+  end
+  planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
